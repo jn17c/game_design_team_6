7c3fc0e9cb303c56596563f25572339b5f1b48b2
stamantr <31514929+stamantr@users.noreply.github.com>
2017-11-02 15:38:19 -0400

    Added shooting functionality
    Added shooting functionality, can be seen in the "game" tab when "gizmos" is activated. It just casts a ray currently and does not shoot anything yet.

---
+++ FLIGHT/Assets/weapon.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weapon : MonoBehaviour {
+
+	public float firerate = 0;
+	public float damage = 10;
+	public LayerMask dontHit;
+
+	float timeToFire = 0;
+	Transform firingpoint;
+	Transform firindestination;
+
+	// Use this for initialization
+	void Awake () {
+		firingpoint = transform.Find("firepoint");
+		firindestination = transform.Find("firingdestination");
+		if (firingpoint == null) {
+			Debug.LogError ("No fire point");
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (firerate == 0) {
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				Shoot();
+			}
+		} else {
+			if(Input.GetKey (KeyCode.Space) && Time.time > timeToFire){
+				timeToFire = Time.time + 1 / firerate;
+				Shoot();
+			}
+		}
+	}
+
+	void Shoot () {
+		Vector2 firedestination = new Vector2 (firindestination.position.x, firindestination.position.y);
+		Vector2 firepoint = new Vector2 (firingpoint.position.x, firingpoint.position.y);
+		RaycastHit2D hit = Physics2D.Raycast (firepoint, firedestination - firepoint, 100, dontHit);
+		Debug.DrawLine (firepoint, firedestination, Color.cyan);
+	}
+}
